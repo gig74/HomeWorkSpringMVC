54cb2da457b7036f86f9c932b03f3dde163c798c
gig74 <gig74_tmp@mail.ru>
2025-02-26 09:11:29 +0500

    02 - Fix bugs

--- src/main/java/org/example/spring/mvc/ServingWebContentApplication.java
+++ src/main/java/org/example/spring/mvc/ServingWebContentApplication.java
@@ -10,9 +10,9 @@ public class ServingWebContentApplication {
 
     public static void main(String[] args) {
 
-//        BooksStorage.getBooks().add( new Book("Улисс", "Джеймс Джойс", 1056));
-//        BooksStorage.getBooks().add( new Book("Война и мир", "Лев Толстой", 1360));
-//        BooksStorage.getBooks().add( new Book("Сага о Форсайтах", "Джон Голсуорси", 1376));
+        BooksStorage.getBooks().add( new Book("Улисс", "Джеймс Джойс", 1056));
+        BooksStorage.getBooks().add( new Book("Война и мир", "Лев Толстой", 1360));
+        BooksStorage.getBooks().add( new Book("Сага о Форсайтах", "Джон Голсуорси", 1376));
 
         SpringApplication.run(ServingWebContentApplication.class, args);
     }
